535b9bd574290fb84213635476dc10a6ec40ea28
huntly-top <richard.aliyev.00@gmail.com>
2024-08-25 17:13:32 +0300

    add ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import { App } from "./app/App";
+import { ErrorBoundary } from "app/providers/ErrorBoundary";
+
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
 );

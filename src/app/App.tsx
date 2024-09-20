95a8cc6c025bb2df8946a8df6477e9c6f1d6c164
huntly-top <richard.aliyev.00@gmail.com>
2024-09-20 16:39:31 +0300

    theme dark

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,18 @@
 import { Header } from "widgets/Header";
 import AppRouter from "./router/AppRouter";
-import "./styles/index.scss";
-import { AppLink } from "shared/ui/AppLink/AppLink";
+import './styles/index.scss'
 import { classNames } from "shared/lib/classNames/classNames";
+import { useEffect } from "react";
+import { useTheme } from "./providers/ThemeProvider";
 
 export function App() {
+
+  const {theme} = useTheme()
+
+  useEffect(() => {
+    // set theme
+    document.body.className = theme;
+  },[theme])
 
     return (
       <div className={classNames('app', {}, [])}>

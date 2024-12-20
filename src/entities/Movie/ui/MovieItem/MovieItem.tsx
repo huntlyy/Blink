bad98857b754c6b395f3fb33b9229304017d11e1
huntly-top <richard.aliyev.00@gmail.com>
2024-12-20 16:56:29 +0300

    re-design MovieItem

--- src/entities/Movie/ui/MovieItem/MovieItem.tsx
+++ src/entities/Movie/ui/MovieItem/MovieItem.tsx
@@ -6,6 +6,7 @@ import { Card } from 'shared/ui/Card/Card';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { useNavigate } from 'react-router-dom';
 import { RatingMovie } from 'shared/ui/RatingMovie/ui/RatingMovie';
+import cls from './MovieItem.module.scss'
 
 interface MovieItemProps {
     className?: string;
@@ -23,16 +24,16 @@ export const MovieItem = memo((props: MovieItemProps) => {
     console.log(data)
 
     return (
-        <div className={classNames('', {}, [className])}>
+        <div className={classNames(cls.MovieItem, {}, [className])}>
              <Card
             onClick={onOpenMovie}
             src={data?.posterUrlPreview}
             alt={data?.nameRu}
             className={classNames('', {}, [className])}
         >
-            <RatingMovie rating={data?.ratingKinopoisk} />
-            <Text title={data?.nameRu} />
-            <Text text={data?.year} />
+            <RatingMovie rating={data?.ratingKinopoisk} className={cls.rating} />
+            <Text title={data?.nameRu} className={cls.info}/>
+            <Text text={data?.year} className={cls.info}/>
         </Card>
         </div>
     );
